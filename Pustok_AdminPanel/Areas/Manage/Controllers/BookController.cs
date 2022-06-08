ebd37c9cbe11fff371ea6ed3af8eb04e2c15c3a8
validash <100703058+validash@users.noreply.github.com>
2022-06-08 14:49:32 +0400

    added crud

---
+++ Pustok_AdminPanel/Areas/Manage/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookController (AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var book = _context.Books.Include(x=>x.Author).Include(x=>x.Genre).ToList();
+            return View(book);
+        }
+    }
+}
---
+++ Pustok_AdminPanel/Models/Slider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 35)]
+        public string Title1 { get; set; }
+        [StringLength(maximumLength: 35)]
+        public string Title2 { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string Text { get; set; }
+        [StringLength(maximumLength: 35)]
+        public string BtnText { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string BtnUrl { get; set; }
+        [StringLength(maximumLength: 100)]
+        public string Image { get; set; }
+    }
+}
